eb01d1c95a6aa4cc1827cf22d036eacbf1c7eacc
kostik4kg <kostik4kg@gmail.com>
2020-10-08 13:15:33 +0300

    feat: factiont decoder

--- src/index.js
+++ src/index.js
@@ -35,10 +35,29 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = [];
+    for(let i = 0; i< expr.length;i+=10){
+        str.push(expr.slice(i, i+10));
+    }
+    let str3=[];
+    for(let j = 0; j<str.length;j++){
+        let str2 = [];
+        for(let t = 0; t<str[j].length; t+=2){
+            str2.push(str[j].slice(t, t + 2))
+        }
+        str3.push(str2);
+    }
+    let str3_trans = str3.map(item => item.map(item => item.replace(/10/g, '.').replace(/11/g, '-')))
+    let str4 = str3_trans.map(item => item.filter(item=>item==='00'? false:true).join(''));
+    let str5 = str4.map(item => MORSE_TABLE[item] );
+    // console.log(str3_trans);
+    // console.log(str4);
+    // console.log(str5.join(''));
+    return str5.join('')
 }
 
 module.exports = {
